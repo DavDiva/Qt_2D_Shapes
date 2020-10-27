270d0bc0b94aedda33d8a2fd5826c29d05b28d7f
Diana Ivanchevska <ivanchevska.diana@gmail.com>
2020-10-27 23:30:57 +0200

    code handles curves dynamically

--- renderarea.cpp
+++ renderarea.cpp
@@ -7,6 +7,7 @@ RenderArea::RenderArea(QWidget *parent) : QWidget(parent),
     mShapeColor(255,255,255),
     mShape(Astroid)
 {
+    on_shape_changed();
 }
 
 QSize RenderArea::minimumSizeHint() const
@@ -18,6 +19,69 @@ QSize RenderArea::sizeHint() const
     return QSize(400,200);
 }
 
+void RenderArea::on_shape_changed()
+{
+    //do something different based on each shape
+     switch (mShape)
+     {
+     case Astroid:
+         //load astroid specific values
+         mScale = 40;
+         mIntervalLength = 2 * M_PI;// M_PI is from <math.h> Pi value up to 20 decimals
+         mStepCount = 256;
+         break;
+
+     case Cycloid:
+
+         break;
+
+     case HuygensCycloid:
+
+         break;
+
+     case HypoCycloid:
+
+         break;
+
+     case FutureCurve:
+         //set specific values
+         break;
+
+     default:
+         break;
+     }
+}
+
+QPointF RenderArea::compute(float t)
+{
+    switch (mShape)
+    {
+    case Astroid:
+        return compute_astroid(t);
+        break;
+
+    case Cycloid:
+        return compute_cycloid(t);
+        break;
+
+    case HuygensCycloid:
+        return compute_huygens(t);
+        break;
+
+    case HypoCycloid:
+        return compute_hypo(t);
+        break;
+
+    case FutureCurve:
+        return compute_future_curve(t);
+        break;
+
+    default:
+        break;
+    }
+    return QPointF(0,0);
+}
+
 QPointF RenderArea::compute_astroid(float t)
 {
     float cos_t = cos(t);
@@ -27,6 +91,22 @@ QPointF RenderArea::compute_astroid(float t)
     return QPointF(x,y);
 }
 
+QPointF RenderArea::compute_cycloid(float t)
+{
+   //TBD
+}
+QPointF RenderArea::compute_huygens(float t)
+{
+   //TBD
+}
+QPointF RenderArea::compute_hypo(float t)
+{
+   //TBD
+}
+QPointF RenderArea::compute_future_curve (float t)
+{
+   //TBD
+}
 
 void RenderArea::paintEvent(QPaintEvent *event)
 {
@@ -36,46 +116,22 @@ void RenderArea::paintEvent(QPaintEvent *event)
 
     painter.setRenderHint(QPainter::Antialiasing,true);
 
-    switch (mShape) {
-    case Astroid:
-        mBackgroundColor = QColorConstants::Red;
-        break;
-
-    case Cycloid:
-        mBackgroundColor = QColorConstants::Green;
-        break;
-
-    case HuygensCycloid:
-        mBackgroundColor = QColorConstants::Blue;
-        break;
-
-    case HypoCycloid:
-        mBackgroundColor = QColorConstants::Yellow;
-        break;
-
-    default:
-        break;
-    }
-
-     painter.setBrush(mBackgroundColor);
-     painter.setPen(mShapeColor);
+    painter.setBrush(mBackgroundColor);
+    painter.setPen(mShapeColor);
 
 
     //drawing area
     painter.drawRect(this->rect());
 
     QPoint center = this->rect().center();
-    int stepCount = 256;
-    float scale = 40;
-    float intervalLength = 2 * M_PI;// M_PI is from <math.h> Pi value up to 20 decimals
-    float step = intervalLength / stepCount;
-    for (float t = 0; t < intervalLength; t+=step )
+    float step = mIntervalLength / mStepCount;
+    for (float t = 0; t < mIntervalLength; t+=step )
     {
-        QPointF point = compute_astroid(t);
+        QPointF point = compute(t);
 
         QPoint pixel;
-        pixel.setX(point.x() * scale + center.x());
-        pixel.setY(point.y() * scale + center.y());
+        pixel.setX(point.x() * mScale + center.x());
+        pixel.setY(point.y() * mScale + center.y());
 
         painter.drawPoint(pixel);
     }
